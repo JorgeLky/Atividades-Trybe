b74168bced6d0b061948b72009aa530bdbc5eb4b
Jorge Lucas <jorge.lucas2264@gmail.com>
2020-05-13 18:51:12 -0300

    maior número adicionado e quantidade de números impares adicionados

--- 4_2/number.js
+++ 4_2/number.js
@@ -1,11 +1,13 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let sum = 0;
 let media = 0;
+let maior = 0;
+let impares = 0;
 
 for (i = 0; i < numbers.length; i++) {
-    console.log(numbers[i])
+    console.log(numbers[i]);
     sum = (sum + numbers[i]);
-    media = sum / numbers.length
+    media = sum / numbers.length;
 }
 
 console.log('a soma é ' + sum)
@@ -16,3 +18,17 @@ if (media > 20) {
 } else {
     console.log("média menor que 20")
 }
+
+for (n in numbers)
+if (numbers[n] > maior) {
+    maior = numbers[n]
+}
+console.log("o número maior é " + maior);
+
+for (m = 0; m < numbers.length; m++) {
+    if (numbers[m] % 2 !== 0){
+        ++impares
+    }
+}
+console.log("existem " + impares + " números ímpares");
+
