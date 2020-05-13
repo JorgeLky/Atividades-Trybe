dfac8d5af442624e16e849eb4457661d214df380
Jorge Lucas <jorge.lucas2264@gmail.com>
2020-05-13 18:04:46 -0300

    código de média adicionado

--- 4_2/number.js
+++ 4_2/number.js
@@ -1,9 +1,12 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let sum = 0;
+let media = 0;
 
 for (i = 0; i < numbers.length; i++) {
     console.log(numbers[i])
     sum = (sum + numbers[i]);
+    media = sum / numbers.length
 }
-console.log(sum)
+console.log('a soma é ' + sum)
+console.log('a média é ' + media)
 
