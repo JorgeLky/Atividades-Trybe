4e9ba56e215ff4266eb8baf99ac0b8c7a7d3bbd6
Jorge Lucas <jorge.lucas2264@gmail.com>
2020-05-28 18:30:06 -0300

    estados adicionados

---
+++ 6_1/script.js
@@ -0,0 +1,15 @@
+// code by Lyraah
+// variáveis e constantes
+const estados = ['Acre (AC)', 'Alagoas (AL)', 'Amapá (AP)', 'Amazonas (AM)', 'Bahia (BA)', 'Ceará (CE)', 'Distrito Federal (DF)', 'Espírito Santo (ES)', 'Goiás (GO)', 'Maranhão (MA)', 'Mato Grosso (MT)', 'Mato Grosso do Sul (MS)', 'Minas Gerais (MG)', 'Pará (PA)', 'Paraíba (PB)', 'Paraná (PR)', 'Pernambuco (PE)', 'Piauí (PI)', 'Rio de Janeiro (RJ)', 'Rio Grande do Norte (RN)', 'Rio Grande do Sul (RS)', 'Rondônia (RO)', 'Roraima (RR)', 'Santa Catarina (SC)', 'São Paulo (SP)', 'Sergipe (SE)', 'Tocantins (TO)'];
+
+// funções e métodos
+function est(n){
+  for(let i = 0; i < n.length; i+= 1){
+      let opt = document.createElement('option');
+      document.querySelector('#estados').appendChild(opt);
+      opt.innerText = n[i];
+  }
+}
+
+// acionamento
+est(estados);
