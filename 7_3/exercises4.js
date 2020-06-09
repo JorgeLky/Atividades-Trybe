3fc4f6999cafb617d92f2787e4e08cd94c932d26
Jorge Lucas <jorge.lucas2264@gmail.com>
2020-06-09 19:36:08 -0300

    exercises part 4 done

---
+++ 7_3/exercises4.js
@@ -0,0 +1,23 @@
+const assert = require('assert');
+
+function myRemove(arr, item) {
+  let newArr = [];
+  for (let i = 0; i < arr.length; i += 1) {
+    if (item !== arr[i]) {
+      newArr.push(arr[i]);
+    }
+  }
+  return newArr;
+}
+
+// implemente seus testes aqui
+
+// 1
+const array = [1, 2, 3, 4];
+assert.deepStrictEqual(myRemove(array, 3), [1, 2, 4]);
+
+// 2
+assert.notEqual(myRemove(array, 3), [1, 2, 3, 4]);
+
+// 3
+assert.deepStrictEqual(myRemove(array), array);
