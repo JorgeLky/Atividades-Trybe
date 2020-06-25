3411aa7b2a9e91762ea6a112d5268448de0a5363
Jorge Lucas <jorge.lucas2264@gmail.com>
2020-06-25 20:59:11 -0300

    test 2 done

--- 8_3/9_1tests/exercise2.spec.js
+++ 8_3/9_1tests/exercise2.spec.js
@@ -1,15 +1,17 @@
-const assert = require('assert')
+const sum = require('../exercise2');
 
-// escreva sum abaixo
 
-const sum = (...numeros) => {
-  const soma = numeros.reduce((total, numero) => total += numero, 0);
-  return (soma);
-}
+// assert.equal(sum(), 0)
+// assert.equal(sum(1), 1)
+// assert.equal(sum(1, 2), 3)
+// assert.equal(sum(1, 2, 3), 6)
+// assert.equal(sum(1, 2, 3, 4), 10)
 
-assert.equal(sum(), 0)
-assert.equal(sum(1), 1)
-assert.equal(sum(1, 2), 3)
-assert.equal(sum(1, 2, 3), 6)
-assert.equal(sum(1, 2, 3, 4), 10)
-    
+test('sum of numbers', () => {
+  expect(sum()).toBe(0);
+  expect(sum(1)).toBe(1);
+  expect(sum(1, 2)).toBe(3);
+  expect(sum(1, 2, 3)).toBe(6);
+  expect(sum(1, 2, 3,)).toBe(6);
+  expect(sum(1, 2, 3, 4)).toBe(10);
+})
