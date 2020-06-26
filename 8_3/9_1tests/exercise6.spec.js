453be505d10ec4386796226fb150f90551244dd7
Jorge Lucas <jorge.lucas2264@gmail.com>
2020-06-25 21:37:38 -0300

    test 6 done

--- 8_3/9_1tests/exercise6.spec.js
+++ 8_3/9_1tests/exercise6.spec.js
@@ -1,13 +1,15 @@
-const assert = require('assert')
+const toObject = require('../exercise6')
 
 const palio = ["Palio", "Fiat", 2019]
 const shelbyCobra = ["Shelby Cobra", "Ford", 1963]
 const chiron = ["Chiron", "Bugatti", 2016]
 
-// escreva toObject abaixo
-const toObject = ([name, brand, year]) => myObj = { name: name, brand: brand, year: year }
+//assert.deepEqual(toObject(palio), { name: "Palio", brand: "Fiat", year: 2019 })
+//assert.deepEqual(toObject(shelbyCobra), { name: "Shelby Cobra", brand: "Ford", year: 1963 })
+//assert.deepEqual(toObject(chiron), { name: "Chiron", brand: "Bugatti", year: 2016 })
 
-assert.deepEqual(toObject(palio), { name: "Palio", brand: "Fiat", year: 2019 })
-assert.deepEqual(toObject(shelbyCobra), { name: "Shelby Cobra", brand: "Ford", year: 1963 })
-assert.deepEqual(toObject(chiron), { name: "Chiron", brand: "Bugatti", year: 2016 })
-
+test('to Object function', () => {
+  expect(toObject(palio)).toEqual({ name: 'Palio', brand: 'Fiat', year: 2019 });
+  expect(toObject(shelbyCobra)).toEqual({ name: "Shelby Cobra", brand: "Ford", year: 1963 });
+  expect(toObject(chiron)).toEqual({ name: "Chiron", brand: "Bugatti", year: 2016 });
+})
