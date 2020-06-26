041ced8ae8c64004b566b56f1b1a3250ddabad55
Jorge Lucas <jorge.lucas2264@gmail.com>
2020-06-25 21:48:57 -0300

    test 8 done

--- 8_3/9_1tests/exercise8.spec.js
+++ 8_3/9_1tests/exercise8.spec.js
@@ -1,10 +1,11 @@
-const assert = require('assert')
+const greet = require('../exercise8')
 
-// escreva greet abaixo
+//assert.equal(greet("John"), "Hi John")
+//assert.equal(greet("John", "Good morning"), "Good morning John")
+//assert.equal(greet("Isabela", "Oi"), "Oi Isabela")
 
-const greet = (name, gretting = 'Hi') => `${gretting} ${name}`
-
-assert.equal(greet("John"), "Hi John")
-assert.equal(greet("John", "Good morning"), "Good morning John")
-assert.equal(greet("Isabela", "Oi"), "Oi Isabela")
-    
+test('greet function', () => {
+  expect(greet('John', 'Good morning')).toEqual('Good morning John');
+  expect(greet('John')).toEqual('Hi John');
+  expect(greet('Isabela', 'Oi')).toEqual('Oi Isabela');
+})
