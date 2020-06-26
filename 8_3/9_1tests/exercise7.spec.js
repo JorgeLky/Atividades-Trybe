f06cc82fb32ed3ab893a0b01c669c16edaf802e1
Jorge Lucas <jorge.lucas2264@gmail.com>
2020-06-25 21:43:27 -0300

    test 7 done

--- 8_3/9_1tests/exercise7.spec.js
+++ 8_3/9_1tests/exercise7.spec.js
@@ -1,4 +1,4 @@
-const assert = require('assert')
+const shipLength = require('../exercise7');
 
 const ships = [
     {
@@ -18,9 +18,13 @@ const ships = [
 ]
 
 // escreva shipLength abaixo
-const shipLength = ({name, length, measurementUnit = 'meters'}) => `${name} is ${length} ${measurementUnit} long`;
 
-assert.equal(shipLength(ships[0]), "Titanic is 269.1 meters long")
-assert.equal(shipLength(ships[1]), "Queen Mary 2 is 1132 feet long")
-assert.equal(shipLength(ships[2]), "Yamato is 256 meters long")
+//assert.equal(shipLength(ships[0]), "Titanic is 269.1 meters long")
+//assert.equal(shipLength(ships[1]), "Queen Mary 2 is 1132 feet long")
+//assert.equal(shipLength(ships[2]), "Yamato is 256 meters long")
     
+test('testing ship length function', () => {
+expect(shipLength(ships[0])).toEqual('Titanic is 269.1 meters long');
+expect(shipLength(ships[1])).toEqual('Queen Mary 2 is 1132 feet long');
+expect(shipLength(ships[2])).toEqual('Yamato is 256 meters long');
+})
