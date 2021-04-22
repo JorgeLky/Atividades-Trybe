c6557bf69da736d4f95ebe7215cad3ed0c83be40
Jorge Lucas <jorge.lucas2264@gmail.com>
2021-04-22 18:05:48 -0300

    exercício 1 completo

---
+++ exercises/algoritmos-e-estrutura-de-dados_4/exercicio1.py
@@ -0,0 +1,13 @@
+from base import Cronometro
+
+## exercicio1 : ["zebra", "macaco", "elefante", "arara", "javali"]
+## após duas iterações com bubble sort:
+## ['elefante', 'arara', 'javali', 'macaco', 'zebra']
+
+## exercício 2:
+## merge sort:
+## 73 54 68 21
+## 37 45 68 12
+## 3457 1268
+## 12345678
+
